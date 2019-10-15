bd11ba3c0d03a277eade88b0bdf82b007aef5225
Winson Bi <wbi002@ucr.edu>
2019-10-15 16:33:59 -0700

    slave v1.0 first try

---
+++ master.c
@@ -0,0 +1,147 @@
+/*	Author: winson
+ *  Partner(s) Name: Dylan han
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <../header/timer.h>
+#include <../header/bit.h>
+#include <../header/io.c>
+#include <../header/keypad.h>
+#include <../header/SPI.c>
+
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned char* patternSelect(unsigned char keypadInput){
+ 	keypadInput = GetKeypadKey();
+	if(keypadInput == 'A'){
+		return "Ptrn: 1";
+	}
+	else if(keypadInput == 'B'){
+		return "Ptrn: 2";
+	}
+	else if(keypadInput == 'C'){
+		return "Ptrn: 3";
+	}
+	else if(keypadInput == 'D'){
+		return "Ptrn: 4";
+	}
+	else{
+		return "Ptrn: Error";
+	}
+}
+
+unsigned char* speedSelect(unsigned char keypadInput){
+	if(keypadInput == '1'){
+		return "Spd: 1";
+	}
+	else if(keypadInput == '2'){
+		return "Spd: 2";
+	}
+	else if(keypadInput == '3'){
+		return "Spd: 3";
+	}
+	else if (keypadInput == '4'){
+		return "Spd: 4";
+	}
+	else{
+		return "Spd: 0";
+	}
+}
+
+unsigned char* slaveSelect(unsigned char keypadInput){
+	if(keypadInput == '7'){
+		return "uC: 1";
+	}
+	else if(keypadInput == '8'){
+		return "uC: 2";
+	}
+	else if(keypadInput == '9'){
+		return "uC: 3";
+	}
+	else{
+		return "uC: 0";
+	}
+}
+
+void CustomLCD_DisplayString( unsigned char column, const unsigned char* string) {//No Clear Screen                       
+   unsigned char c = column;                                                                          
+   while(*string) {
+      LCD_Cursor(c++);
+      LCD_WriteData(*string++);                                                                       
+   }                                                                                                  
+}   
+unsigned char tempA = 0x00;
+unsigned char* currentPattern = "Ptrn: 0";
+unsigned char* currentSpeed = "Spd: 0";
+unsigned char* currentSlave = "uC: 0";
+unsigned char keypadInput = 0x00;
+unsigned char cData = 0x00;
+unsigned int slave = 0;
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0xF0; PORTA = 0x0F;
+	DDRB = 0xFF; PORTB = 0x00; //SLAVE SELECT ON MASTER uC
+	DDRC = 0xFF; PORTC = 0x00; //OUTPUTS
+	DDRD = 0xFF; PORTD = 0x00; //OUTPUTS;
+    /* Insert your solution below */
+   
+	LCD_init();
+	LCD_ClearScreen();
+	SPI_MasterInit();
+	TimerOn();
+	TimerSet(100);
+	CustomLCD_DisplayString(1,currentPattern);
+	CustomLCD_DisplayString(10,currentSpeed);
+	CustomLCD_DisplayString(17,currentSlave);
+    	while (1) {
+		
+		tempA = PINA;
+		keypadInput = GetKeypadKey();
+		if(keypadInput != '\0'){
+			//update screen and send signal
+			if(keypadInput == 'A' || keypadInput == 'B' || keypadInput  == 'C' || keypadInput == 'D'){
+				currentPattern = patternSelect(keypadInput);
+				CustomLCD_DisplayString(1,currentPattern);
+				if(slave != 0){
+					SPI_MasterTransmit(keypadInput, PORTB, slave);			
+				}
+			}
+			else if(keypadInput == '1' || keypadInput == '2' || keypadInput == '3' || keypadInput == '4'){
+				currentSpeed = speedSelect(keypadInput);
+				CustomLCD_DisplayString(10,currentSpeed);
+				if(slave != 0){
+					SPI_MasterTransmit(keypadInput, PORTB, slave);
+				}
+			}
+			else if(keypadInput == '7' || keypadInput == '8' || keypadInput == '9'){
+				currentSlave = slaveSelect(keypadInput);
+				CustomLCD_DisplayString(17,currentSlave);
+				if(keypadInput == '7'){
+					slave = 1;
+				}
+				if(keypadInput == '8'){
+					slave = 2;
+				}
+				if(keypadInput == '9'){
+					slave = 3;
+				}
+			}
+			else{//invalid key don't update screen
+				break;
+			}
+		}
+		//LCD_Cursor(1);
+		//LCD_WriteData(GetKeypadKey());
+		while(!TimerFlag);
+		TimerFlag = 0;
+	
+    	}
+    	return 1;
+}
--- source/main.c
+++ source/main.c
@@ -18,75 +18,11 @@
 #include "simAVRHeader.h"
 #endif
 
-unsigned char* patternSelect(unsigned char keypadInput){
- 	keypadInput = GetKeypadKey();
-	if(keypadInput == 'A'){
-		return "Ptrn: 1";
-	}
-	else if(keypadInput == 'B'){
-		return "Ptrn: 2";
-	}
-	else if(keypadInput == 'C'){
-		return "Ptrn: 3";
-	}
-	else if(keypadInput == 'D'){
-		return "Ptrn: 4";
-	}
-	else{
-		return "Ptrn: Error";
-	}
-}
-
-unsigned char* speedSelect(unsigned char keypadInput){
-	if(keypadInput == '1'){
-		return "Spd: 1";
-	}
-	else if(keypadInput == '2'){
-		return "Spd: 2";
-	}
-	else if(keypadInput == '3'){
-		return "Spd: 3";
-	}
-	else if (keypadInput == '4'){
-		return "Spd: 4";
-	}
-	else{
-		return "Spd: 0";
-	}
-}
-
-unsigned char* slaveSelect(unsigned char keypadInput){
-	if(keypadInput == '7'){
-		return "uC: 1";
-	}
-	else if(keypadInput == '8'){
-		return "uC: 2";
-	}
-	else if(keypadInput == '9'){
-		return "uC: 3";
-	}
-	else{
-		return "uC: 0";
-	}
-}
-
-void CustomLCD_DisplayString( unsigned char column, const unsigned char* string) {//No Clear Screen                       
-   unsigned char c = column;                                                                          
-   while(*string) {
-      LCD_Cursor(c++);
-      LCD_WriteData(*string++);                                                                       
-   }                                                                                                  
-}   
 unsigned char tempA = 0x00;
-unsigned char* currentPattern = "Ptrn: 0";
-unsigned char* currentSpeed = "Spd: 0";
-unsigned char* currentSlave = "uC: 0";
-unsigned char keypadInput = 0x00;
 unsigned char cData = 0x00;
-unsigned int slave = 0;
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRA = 0xF0; PORTA = 0x0F;
+	DDRA = 0xFF; PORTA = 0x00;
 	DDRB = 0xFF; PORTB = 0x00; //SLAVE SELECT ON MASTER uC
 	DDRC = 0xFF; PORTC = 0x00; //OUTPUTS
 	DDRD = 0xFF; PORTD = 0x00; //OUTPUTS;
@@ -94,51 +30,18 @@ int main(void) {
    
 	LCD_init();
 	LCD_ClearScreen();
-	SPI_MasterInit();
+	SPI_ServantInit();
 	TimerOn();
 	TimerSet(100);
-	CustomLCD_DisplayString(1,currentPattern);
-	CustomLCD_DisplayString(10,currentSpeed);
-	CustomLCD_DisplayString(17,currentSlave);
     	while (1) {
 		
-		tempA = PINA;
-		keypadInput = GetKeypadKey();
-		if(keypadInput != '\0'){
-			//update screen and send signal
-			if(keypadInput == 'A' || keypadInput == 'B' || keypadInput  == 'C' || keypadInput == 'D'){
-				currentPattern = patternSelect(keypadInput);
-				CustomLCD_DisplayString(1,currentPattern);
-				if(slave != 0){
-					SPI_MasterTransmit(keypadInput, PORTB, slave);			
-				}
-			}
-			else if(keypadInput == '1' || keypadInput == '2' || keypadInput == '3' || keypadInput == '4'){
-				currentSpeed = speedSelect(keypadInput);
-				CustomLCD_DisplayString(10,currentSpeed);
-				if(slave != 0){
-					SPI_MasterTransmit(keypadInput, PORTB, slave);
-				}
-			}
-			else if(keypadInput == '7' || keypadInput == '8' || keypadInput == '9'){
-				currentSlave = slaveSelect(keypadInput);
-				CustomLCD_DisplayString(17,currentSlave);
-				if(keypadInput == '7'){
-					slave = 1;
-				}
-				if(keypadInput == '8'){
-					slave = 2;
-				}
-				if(keypadInput == '9'){
-					slave = 3;
-				}
-			}
-			else{//invalid key don't update screen
-				break;
-			}
+		if(receivedData == 0x00){
+			tempA = 0x00;
 		}
-		//LCD_Cursor(1);
-		//LCD_WriteData(GetKeypadKey());
+		else{
+			tempA = 0x01;
+		}
+		PORTA = tempA;
 		while(!TimerFlag);
 		TimerFlag = 0;
 	
